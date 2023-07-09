9cd50c29d5e727ebe7a8485c5379115a8d740341
seongheung.bae <seongheung.bae@toss.im>
2023-07-09 13:20:38 +0900

    refactor: 코드 정리

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -33,24 +33,4 @@ fun Application.module() {
     configureSerialization()
     configureDependencyInjection()
     configureRouting()
-    install(WebSockets) {
-        pingPeriod = Duration.ofSeconds(15)
-        timeout = Duration.ofSeconds(15)
-        maxFrameSize = Long.MAX_VALUE
-        masking = false
-    }
-
-    routing {
-        webSocket("/echo") {
-            for (frame in incoming) {
-                frame as? Frame.Text ?: continue
-                val receivedText = frame.readText()
-                if (receivedText.equals("bye", ignoreCase = true)) {
-                    close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
-                } else {
-                    send(Frame.Text("Hi, $receivedText!"))
-                }
-            }
-        }
-    }
 }
--- src/main/kotlin/com/example/api/util/RequestUtils.kt
+++ src/main/kotlin/com/example/api/util/RequestUtils.kt
@@ -7,19 +7,17 @@ import io.ktor.server.application.ApplicationCall
 
 object RequestUtils {
     val mapper = jacksonObjectMapper()
-    fun ApplicationCall.getEntityId() =
-        this.parameters["id"]?.toLong() ?: throw IllegalArgumentException()
+
+    fun ApplicationCall.getEntityId(): Long =
+        parameters["id"]?.toLong() ?: throw IllegalArgumentException("The 'id' parameter is missing")
 
     inline fun <reified T : Any> Parameters.toClass(): T {
-        val map = entries().associate {
-            it.key to (it.value.getOrNull(0)
-                ?: throw IllegalArgumentException("Missing value for key ${it.key}"))
-        }
-        return mapper.readValue(mapper.writeValueAsString(map), T::class.java)
+        val map = entries().associate { it.key to (it.value.getOrNull(0) ?: throw IllegalArgumentException("Missing value for key ${it.key}")) }
+        return mapper.convertValue(map, T::class.java)
     }
 
     inline fun <reified T : Any> ApplicationCall.getQueryParams(): T {
-        return this.request.queryParameters.toClass<T>()
+        return this.request.queryParameters.toClass()
     }
-
 }
+
